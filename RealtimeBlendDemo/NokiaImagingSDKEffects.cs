97bb9a005af1ed4411d294288d76dd0b3cd2a672
Sakari Hyoty <sakari.hyoty@futurice.com>
2014-02-25 10:54:11 +0200

    Fix: Effect indexing in UI is incorrect (index 15 shows twice)

--- RealtimeBlendDemo/NokiaImagingSDKEffects.cs
+++ RealtimeBlendDemo/NokiaImagingSDKEffects.cs
@@ -208,111 +208,111 @@ namespace RealtimeBlendDemo
             {
                 case 0:
                     {
-                        EffectName = String.Format(nameFormat, 1, AppResources.Filter_None);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_None);
                     }
                     break;
 
                 case 1:
                     {
-                        EffectName = String.Format(nameFormat, 2, AppResources.Filter_Blend_Normal);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Normal);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Normal, EffectLevel);
                     }
                     break;
 
                 case 2:
                     {
-                        EffectName = String.Format(nameFormat, 3, AppResources.Filter_Blend_Multiply);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Multiply);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Multiply, EffectLevel);
                     }
                     break;
 
                 case 3:
                     {
-                        EffectName = String.Format(nameFormat, 4, AppResources.Filter_Blend_Add);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Add);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Add, EffectLevel);
                     }
                     break;
 
                 case 4:
                     {
-                        EffectName = String.Format(nameFormat, 5, AppResources.Filter_Blend_Color);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Color);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Color, EffectLevel);
                     }
                     break;
 
                 case 5:
                     {
-                        EffectName = String.Format(nameFormat, 6, AppResources.Filter_Blend_Colorburn);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Colorburn);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Colorburn, EffectLevel);
                     }
                     break;
 
                 case 6:
                     {
-                        EffectName = String.Format(nameFormat, 7, AppResources.Filter_Blend_Colordodge);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Colordodge);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Colordodge, EffectLevel);
                     }
                     break;
 
                 case 7:
                     {
-                        EffectName = String.Format(nameFormat, 8, AppResources.Filter_Blend_Overlay);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Overlay);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Overlay, EffectLevel);
                     }
                     break;
 
                 case 8:
                     {
-                        EffectName = String.Format(nameFormat, 9, AppResources.Filter_Blend_Softlight);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Softlight);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Softlight, EffectLevel);
                     }
                     break;
 
                 case 9:
                     {
-                        EffectName = String.Format(nameFormat, 10, AppResources.Filter_Blend_Screen);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Screen);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Screen, EffectLevel);
                     }
                     break;
 
                 case 10:
                     {
-                        EffectName = String.Format(nameFormat, 11, AppResources.Filter_Blend_Hardlight);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Hardlight);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Hardlight, EffectLevel);
                     }
                     break;
 
                 case 11:
                     {
-                        EffectName = String.Format(nameFormat, 12, AppResources.Filter_Blend_Darken);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Darken);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Darken, EffectLevel);
                     }
                     break;
 
                 case 12:
                     {
-                        EffectName = String.Format(nameFormat, 13, AppResources.Filter_Blend_Lighten);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Lighten);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Lighten, EffectLevel);
                     }
                     break;
 
                 case 13:
                     {
-                        EffectName = String.Format(nameFormat, 14, AppResources.Filter_Blend_Hue);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Hue);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Hue, EffectLevel);
                     }
                     break;
 
                 case 14:
                     {
-                        EffectName = String.Format(nameFormat, 15, AppResources.Filter_Blend_Exclusion);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Exclusion);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Exclusion, EffectLevel);
                     }
                     break;
 
                 case 15:
                     {
-                        EffectName = String.Format(nameFormat, 15, AppResources.Filter_Blend_Difference);
+                        EffectName = String.Format(nameFormat, _effectIndex + 1, AppResources.Filter_Blend_Difference);
                         _blendFilter = new BlendFilter(_blendImageProvider, BlendFunction.Difference, EffectLevel);
                     }
                     break;
